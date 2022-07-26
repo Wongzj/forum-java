1552c96135733af28e42de44f8e1976315a2bd58
wangzhijian.connor <wangzhijian.connor@bytedance.com>
2022-07-26 21:39:33 +0800

    modification 2

--- forum-infrastructure/src/main/java/pub/developers/forum/infrastructure/transfer/ConfigTransfer.java
+++ forum-infrastructure/src/main/java/pub/developers/forum/infrastructure/transfer/ConfigTransfer.java
@@ -10,11 +10,6 @@ import pub.developers.forum.infrastructure.dal.dataobject.ConfigDO;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author Qiangqiang.Bian
- * @create 2020/12/26
- * @desc
- **/
 public class ConfigTransfer {
 
     public static List<Config> toConfigs(List<ConfigDO> configDOList) {
